475cb88035e8edb9496ed667d41b58dec906e427
suryawanshi-ankit <suryawanshiankit97@gmail.com>
2024-09-28 10:04:24 +0530

    exploring git post delete

--- src/app.js
+++ src/app.js
@@ -2,19 +2,25 @@ const express = require('express');
 
 const app = express();
 
-app.use('/hello', (req, res) => {
-    console.log('Hello route')
-    res.send('Hello route');
+app.get('/user', (req, res) => {
+    console.log('test route')
+    res.send({
+        fname: 'ankit',
+        lname: 'suryawanshi'
+    });
+});
+
+app.post('/user', (req, res) => {
+    res.send('New user created');
+});
+
+app.delete('/user', (req, res) => {
+    res.send('User deleted');
 });
 
 app.use('/test', (req, res) => {
     console.log('test route')
     res.send('test route');
-});
-
-app.use('/', (req, res) => {
-    console.log('Hello from the server');
-    res.send('Hello from the server');
 });
 
 app.listen(3000, () => console.log('server is listening on 30000'));
