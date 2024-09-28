780363926b4277a33f2aebf9474d7b4e7db2d845
suryawanshi-ankit <suryawanshiankit97@gmail.com>
2024-09-28 22:05:24 +0530

    get API

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/database');
-const User =require('./models/user');
+const User = require('./models/user');
 
 const app = express();
 
@@ -8,12 +8,36 @@ app.use(express.json());
 
 app.post('/signup', async (req, res) => {
     // creating new instance of the user model
-    const userData = new User(req.body)
-
+    const userData = new User(req.body);
     try {
         await userData.save();
         res.send("User created successfully !!!");
-    } catch(err) {
+    } catch (err) {
+        res.status(400).send("Error saving the user: ", err.message)
+    }
+
+})
+
+app.get('/user', async (req, res) => {
+    const firstName = req.body.firstName;
+    try {
+        const users = await User.find({firstName: firstName});
+        if (users?.length === 0) {
+            res.status(404).send('User not found');
+        } else {
+            res.send(users);
+        }
+    } catch (err) {
+        res.status(400).send("Error saving the user: ", err.message)
+    }
+
+})
+
+app.get('/feed', async (req, res) => {
+    try {
+        const data = await User.find({});
+        res.send(data);
+    } catch (err) {
         res.status(400).send("Error saving the user: ", err.message)
     }
 
