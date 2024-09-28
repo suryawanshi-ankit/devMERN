d96bad469c356b0fe6de79fdfbd5535a07bc18bb
suryawanshi-ankit <suryawanshiankit97@gmail.com>
2024-09-28 14:19:29 +0530

    playing with multiple routes

--- src/app.js
+++ src/app.js
@@ -2,25 +2,32 @@ const express = require('express');
 
 const app = express();
 
-app.get('/user', (req, res) => {
-    console.log('test route')
-    res.send({
-        fname: 'ankit',
-        lname: 'suryawanshi'
-    });
-});
-
-app.post('/user', (req, res) => {
-    res.send('New user created');
-});
-
-app.delete('/user', (req, res) => {
-    res.send('User deleted');
-});
-
-app.use('/test', (req, res) => {
-    console.log('test route')
-    res.send('test route');
-});
+app.use('/user',
+    (req, res, next) => {
+        console.log('user 1St route')
+        next();
+        // res.send('1st route');
+    },
+    (req, res, next) => {
+        console.log('user 2nd route');
+        next();
+        // res.send('2nd route');
+    },
+    (req, res, next) => {
+        console.log('user 3rd route');
+        next();
+        // res.send('3rd route');
+    },
+    (req, res, next) => {
+        console.log('user 4th route');
+        next();
+        res.send('4th route');
+    },
+    (req, res, next) => {
+        console.log('user 5th route');
+        next();
+        res.send('5th route');
+    },
+);
 
 app.listen(3000, () => console.log('server is listening on 30000'));
