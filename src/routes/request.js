90308f74c3e3287861e57ee1b856f84789976214
suryawanshi-ankit <suryawanshiankit97@gmail.com>
2024-09-30 23:04:27 +0530

    created connectionRequestSchema + request send API

---
+++ src/routes/request.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const { userAuth } = require('../middlewares/auth');
+const ConnectionRequest = require('../models/connectionRequest');
+const User = require('../models/user');
+
+const requestRouter = express.Router();
+
+requestRouter.post('/request/send/:status/:userId', userAuth, async (req, res) => {
+    try {
+        const fromUserId = req.user._id;
+        const toUserId = req.params.userId;
+        const status = req.params.status;
+
+        const acceptedStatus = ['ignored', 'interested'];
+
+        if (!acceptedStatus.includes(status)) throw new Error(`Status ${status} is not valid !!!`);
+
+        const toUser = await User.findById(toUserId);
+
+        if (!toUser) {
+            return res.status(404).json({message: 'User not found!!!'})
+        }
+        const isRequestExists = await ConnectionRequest.findOne({
+            $or: [
+                {fromUserId, toUserId},
+                {fromUserId: toUserId, toUserId: fromUserId}
+            ]
+        })
+
+        if (isRequestExists) throw new Error(`Request already exists`);
+
+        const connectionRequest = new ConnectionRequest({
+            fromUserId,
+            toUserId,
+            status
+        });
+        const data = await connectionRequest.save();
+        res.json({message: `${status} request is sended`, data})
+    } catch (err) {
+        res.status(400).send(`ERROR: ${err.message}`);
+    }
+})
+
+// requestRouter.post('', (req, res) => {
+//     try {
+        
+//     } catch (error) {
+        
+//     }
+// })
+
+// requestRouter.post('', (req, res) => {
+//     try {
+        
+//     } catch (error) {
+        
+//     }
+// })
+
+module.exports = requestRouter;
